0dee8b96beb0cd36f1992282cba4632a99b82ff2
Julián Gutiérrez <38226781+julianfelipe27@users.noreply.github.com>
2020-02-28 10:32:49 -0500

    Blank space added

--- test/GithubApi.Authentication.test.js
+++ test/GithubApi.Authentication.test.js
@@ -16,6 +16,7 @@ describe('Github Api Test', () => {
       expect(response.body.description).equal('This is a Workshop about Api Testing in JavaScript');
     });
   });
+  
   it('Via OAuth2 Tokens by parameter', () => agent.get(`${urlBase}/repos/${githubUserName}/${repository}`)
     .query(`access_token=${process.env.ACCESS_TOKEN}`)
     .set('User-Agent', 'agent')
